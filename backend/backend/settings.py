045914d1a91f0c74488fe61c0b40f0a82de908a7
Beniamino Vagnarelli <beniaminovagnarelli@gmail.com>
2024-03-26 23:11:20 +0100

    removing .pgpass and getting password with environment variables

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,10 +77,15 @@ WSGI_APPLICATION = "backend.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "OPTIONS": {
-            "service": "db_service",
-            "passfile": ".pgpass",
-        },
+        # "OPTIONS": {
+        #     "service": "db_service",
+        #     "passfile": ".pgpass",
+        # },
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
