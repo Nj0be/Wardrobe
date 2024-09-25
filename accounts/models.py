7a5d0550ad7479009a0f587e93b270db134e2cb3
Beniamino Vagnarelli <beniaminovagnarelli@gmail.com>
2024-09-25 10:57:27 +0200

    Modifiche settings.py per includere le varie app e modifiche a progetto pycharm

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
-# class User(AbstractUser):
-    # username = None
-    # phone = models.CharField(max_length=25, blank=True, unique=True, null=True)
+class Customer(models.Model):
+    user = models.OneToOneField(models.User, on_delete=models.CASCADE)
+    username = None
+    phone = models.CharField(max_length=25, blank=True, unique=True, null=True)
 
-    # def __str__(self):
-        # return self.username
+    def __str__(self):
+        return self.username
+
+class Employee(models.Model):
+    user = models.OneToOneField(models.User, on_delete=models.CASCADE)
+
 
 # class CartItem(models.Model):
 
