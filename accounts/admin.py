0c9f2208252db2a0dd06bd27bd6f90a057b4e89b
Beniamino Vagnarelli <beniaminovagnarelli@gmail.com>
2024-09-28 18:46:54 +0200

    Added User model with custom authentication (email instead of username)

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class CartProductInline(admin.TabularInline):
+    model = CartProduct
+
+
+@admin.register(User)
+class AuthorAdmin(admin.ModelAdmin):
+    inlines = [CartProductInline]
--- accounts/models.py
+++ accounts/models.py
@@ -1,19 +1,63 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+from django.db.models import ManyToManyField
 
-class Customer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+from products.models import ProductVariant
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
     username = None
-    phone = models.CharField(max_length=25, blank=True, unique=True, null=True)
+    email = models.EmailField(_('email address'), unique=True)
+
+    # REQUIRED_FIELDS must contain all required fields on your user model, but should not contain the USERNAME_FIELD
+    # or password as these fields will always be prompted for.
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    # Custom fields here
+
 
     def __str__(self):
-        return self.username
-
-class Employee(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+        return self.email
 
 
-# class CartItem(models.Model):
-
-
-# class Cart(models.Model):
+class CartProduct(models.Model):
+    product = models.ForeignKey(ProductVariant, on_delete=models.CASCADE)
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
