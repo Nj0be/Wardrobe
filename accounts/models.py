acfaa5674fb3c5c9d6c163d62de25bc168bb5132
Beniamino Vagnarelli <beniaminovagnarelli@gmail.com>
2024-10-01 12:51:51 +0200

    Renamed product to product_variant inside CartItem

--- accounts/models.py
+++ accounts/models.py
@@ -55,14 +55,14 @@ class User(AbstractUser):
 
 
 class CartItem(models.Model):
-    product = models.ForeignKey('products.ProductVariant', on_delete=models.CASCADE)
+    product_variant = models.ForeignKey('products.ProductVariant', on_delete=models.CASCADE)
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(validators=[MinValueValidator(1)])
     is_active = models.BooleanField(default=True)
 
     def clean(self):
         # Don't add product to cart if stock == 0
-        self.quantity = min(self.quantity, self.product.stock)
+        self.quantity = min(self.quantity, self.product_variant.stock)
         if self.quantity == 0:
             raise ValidationError(_("Can't add Product to cart with quantity = 0"))
 
